d0a6285237dd5c2a1ca4fd892f38ba49d6f6894e
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-07-24 23:03:28 +0200

    feat: add neural network architecture for logistic regression

---
+++ logistic_regression_with_backpropagation.py
@@ -0,0 +1,28 @@
+from typing import Tuple
+import numpy as np
+
+class LogisticRegression:
+    def __init__(self, n_dimentions: int) -> None:
+        self.weights = np.zeros(shape=(n_dimentions, 1))
+        self.bias = 0
+    
+    def predict(self, X: np.array) -> np.array:
+        pass
+
+    def train_model(self, X: np.array, y: np.array, n_iterations: int, learning_rate: float) -> None:
+        pass
+    
+    def _sigma_function(self, Z: np.array) -> np.array:
+        """Vectorized function: 1 / (1 + exp(-z))"""
+        pass
+    
+    def _backpropagate_values(self, weights: np.array, bias: float, X: np.array, Y: np.array) -> Tuple[float, float]:
+        """
+        Returns derivatives:
+            dw: np.array
+            db: np.array 
+        """
+        pass
+
+    def _optimize_parameters(self, weights: np.array, bias: float, n_iterations: int, learning_rate: float) -> Tuple[np.array, float]:
+        pass
