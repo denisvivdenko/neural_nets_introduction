3899d838977ce37f93eaed84057fedc9c848d97d
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-07-24 23:08:01 +0200

    feat: add prediction method

--- logistic_regression_with_backpropagation.py
+++ logistic_regression_with_backpropagation.py
@@ -2,12 +2,14 @@ from typing import Tuple
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self, n_dimentions: int) -> None:
+    def __init__(self, n_dimentions: int, threshold: float = 0.5) -> None:
+        self.threshold = threshold
         self.weights = np.zeros(shape=(n_dimentions, 1))
         self.bias = 0
     
     def predict(self, X: np.array) -> np.array:
-        pass
+        predictions = np.dot(self.weights.T, X) + self.bias
+        return np.array([1 if prediction > self.threshold else 0 for prediction in predictions])
 
     def train_model(self, X: np.array, y: np.array, n_iterations: int, learning_rate: float) -> None:
         pass
