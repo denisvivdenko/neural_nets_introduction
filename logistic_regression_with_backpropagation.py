ed60d726900c16a78e8a8f7bae63bcf1cfe7b35a
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-07-25 22:32:47 +0200

    feat: normalize input vectors and randomly initialize weights

--- logistic_regression_with_backpropagation.py
+++ logistic_regression_with_backpropagation.py
@@ -7,7 +7,7 @@ import h5py
 class LogisticRegression:
     def __init__(self, n_dimentions: int, threshold: float = 0.5) -> None:
         self.threshold = threshold
-        self.weights = np.zeros(shape=(n_dimentions, 1))
+        self.weights = (.5 - np.random.rand(n_dimentions)).reshape(n_dimentions, 1)
         self.bias = 0
     
     def predict(self, X: np.array) -> np.array:
@@ -25,17 +25,22 @@ class LogisticRegression:
         """
         m - number of datapoints, n - number of features.
         weights.shape = (n, 1)
-        X.shape = (m, n)
-        Y.shape = (m, 1)
+        X.shape = (n, m)
+        Y.shape = (1, m)
 
         Returns derivatives:
             dw: np.array (shape = (n, 1))
             db: float
         """
-        n_records = X.shape[0]
-        Z = np.dot(X, weights) + bias  # Z.shape = (m, 1)
-        A = self._sigma_function(Z)  # A.shape = (m, 1)
-        dw = np.dot(X.T, (A - Y)) / n_records  # dw.shape = (n, 1)
+        n_records = X.shape[1]
+        Z = np.dot(weights.T, X) + bias  # Z.shape = (1, m)
+        A = self._sigma_function(Z)  # A.shape = (1, m)
+
+        # print(A)
+        cost_function = np.sum(Y * np.log(A) + (1 - Y) * np.log(1 - A))
+        print(f"cost: {cost_function}")
+
+        dw = np.dot(X, (A - Y).T) / n_records  # dw.shape = (n, 1)
         db = np.sum((A - Y)) / n_records
         return (dw, db)
 
@@ -44,6 +49,7 @@ class LogisticRegression:
             dw, db = self._backpropagate_values(weights, bias, X, Y)
             weights = weights - learning_rate * dw
             bias = bias - learning_rate * db
+            # print(f"weights: {weights} || bias: {bias}")
         return weights, bias
 
 
@@ -53,14 +59,22 @@ def read_h5(file_path: str) -> Tuple[np.array, np.array]:
         return file[X_key][()], file[Y_key][()]
 
 
+def normalize_array(array: np.array) -> np.array:
+    norm_vector = (np.linalg.norm(array, axis=1)).reshape(-1, 1)
+    return array / norm_vector
+    
+
 if __name__ == "__main__":
     train_dataset_path = "./dataset/train_catvnoncat.h5"
     test_dataset_path = "./dataset/test_catvnoncat.h5"
     X_train, Y_train = read_h5(train_dataset_path)
     X_test, Y_test = read_h5(test_dataset_path)
     print(f"X_train.shape = {X_train.shape}. X_test.shape = {X_test.shape}\nY_train.shape = {Y_train.shape}. Y_test.shape = {Y_test.shape}")
-    X_train = X_train.reshape(X_train.shape[0], -1).T
-    X_test = X_test.reshape(X_test.shape[0], -1).T
-    Y_train, Y_test = Y_train.reshape(-1, 1).T, Y_test.reshape(-1, 1).T
+    X_train = normalize_array(X_train.reshape(X_train.shape[0], -1).T)  # shape (n, m)
+    X_test = normalize_array(X_test.reshape(X_test.shape[0], -1).T)  # shape (n, m_test)
+    Y_train, Y_test = Y_train.reshape(-1, 1).T, Y_test.reshape(-1, 1).T  # shape (1, m), shape (1, m_test)
     print("RESHAPING DATASET...")
     print(f"X_train.shape = {X_train.shape}. X_test.shape = {X_test.shape}\nY_train.shape = {Y_train.shape}. Y_test.shape = {Y_test.shape}")
+
+    model = LogisticRegression(n_dimentions=X_train.shape[0])
+    model.train_model(X_train, Y_train, n_iterations=10000, learning_rate=0.1)
