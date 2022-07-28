9d9bcde01b21e4a4c0eeeb9d78ed27f7b8a0d7b7
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-07-28 22:02:46 +0200

    feat: add backpropagation

--- neural_network.py
+++ neural_network.py
@@ -15,13 +15,13 @@ class NeuralNetwork:
         n_output_units = X.shape[0]
         parameters = self.initialize_parametres(n_input_units, self.n_hidden_units, n_output_units)
         for epoch in range(n_epoch):
-            A_output, cost, cache = self.forward_propagation(parameters, X)
-            gradients = self.backward_propagation(cache, parameters)
+            A_output, cache = self.forward_propagation(parameters, X)
+            gradients = self.backward_propagation(cache, parameters, X, Y)
             parameters = self.update_parameters(parameters, gradients)
-            if epoch % 10 == 0: print(f"Cost: {cost}")
+            # if epoch % 10 == 0: print(f"Cost: {cost}")
         self.parameters = parameters        
 
-    def forward_propagation(self, parameters: dict, X: np.array) -> Tuple[float, float, dict]:
+    def forward_propagation(self, parameters: dict, X: np.array) -> Tuple[float, dict]:
         """Returns A[L], Cost and cached values Z[l] and A[l] in dictionary."""
         W1, W2 = parameters["W1"], parameters["W2"]
         b1, b2 = parameters["b1"], parameters["b2"]
@@ -29,11 +29,34 @@ class NeuralNetwork:
         A1 = np.tanh(Z1)
         Z2 = np.dot(W2, A1) + b2
         A2 = self.sigma(Z2)
-        return A2
+        cache = {
+            "Z1": Z1,
+            "A1": A1,
+            "Z2": Z2,
+            "A2": A2
+        }
+        return A2, cache
 
     def backward_propagation(self, cache: dict, parameters: dict, X: np.array, Y: np.array) -> dict:
         """Computes gradients for parameters and returns dictionary with dW1, db1, dW2, db2 values."""
-        pass
+        m = X.shape[1]
+        W1, W2 = parameters["W1"], parameters["W2"]  # W1.shape = (n1, nx); W2.shape = (n2, n1)
+        A1, A2 = cache["A1"], cache["A2"]  # A1.shape = (n1, m); A2.shape = (n2, m)
+        Z1, Z2 = cache["Z1"], cache["Z2"]  # Z1.shape = (n1, m); Z2.shape = (n2, m)
+
+        dZ2 = A2 - Y  # (n2, m)
+        dW2 = np.dot(dZ2, A1.T) / m  # (n2, n1)  
+        db2 = np.sum(dZ2, axis=1, keepdims=True) / m  # (n2, 1)
+        dZ1 = np.dot(W2.T, dZ2) * (1 - np.power(A1, 2))  # (n1, m)
+        dW1 = np.dot(dZ1, X.T) / m  # (n1, nx)
+        db1 = np.sum(dZ1, axis=1, keepdims=True) / m  # (n1, 1)
+        gradients = {
+            "dW1": dW1,
+            "dW2": dW2,
+            "db1": db1,
+            "db2": db2
+        }
+        return gradients
 
     def update_parameters(self, parameters: dict, gradients: dict) -> dict:
         """Returns dict with updated parameters"""
