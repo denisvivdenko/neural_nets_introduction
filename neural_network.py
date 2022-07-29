eaa21c6f96bb51445c12db12a2d9e86d761e58ec
denisvivdenko <denis.vivdenko.freelancer@gmail.com>
2022-07-29 10:43:03 +0200

    feat: load dataset

--- neural_network.py
+++ neural_network.py
@@ -1,5 +1,8 @@
 import numpy as np
 from typing import Tuple
+import matplotlib.pyplot as plt
+
+from dataset.planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
 
 
 class NeuralNetwork:
@@ -10,7 +13,7 @@ class NeuralNetwork:
     def predict(self, X: np.array, parameters: dict = None) -> np.array:
         pass
 
-    def train_model(self, X: np.array, Y: np.array, n_epoch: int = 100, learning_rate: float = 0.05) -> None:
+    def train_model(self, X: np.array, Y: np.array, n_epoch: int = 1000, learning_rate: float = 0.05) -> None:
         n_input_units = X.shape[0]
         n_output_units = X.shape[0]
         parameters = self.initialize_parametres(n_input_units, self.n_hidden_units, n_output_units)
@@ -18,7 +21,8 @@ class NeuralNetwork:
             A_output, cache = self.forward_propagation(parameters, X)
             gradients = self.backward_propagation(cache, parameters, X, Y)
             parameters = self.update_parameters(parameters, gradients, learning_rate)
-            if epoch % 10 == 0: print(f"Cost: {self.compute_cost(A_output, Y)}")
+            if epoch % 1000 == 0: print(f"Cost: {self.compute_cost(A_output, Y)}")
+        print(f"Final cost: {self.compute_cost(A_output, Y)}")
         self.parameters = parameters        
 
     def forward_propagation(self, parameters: dict, X: np.array) -> Tuple[float, dict]:
@@ -60,13 +64,13 @@ class NeuralNetwork:
     def update_parameters(self, parameters: dict, gradients: dict, learning_rate: float) -> dict:
         """Returns dict with updated parameters"""
         updated_parameters = {}
-        W1, W2  = parameters["w1"], parameters["W2"]
+        W1, W2  = parameters["W1"], parameters["W2"]
         dW1, dW2 = gradients["dW1"], gradients["dW2"]
         db1, db2 = gradients["db1"], gradients["db2"]
         updated_parameters["W1"] = W1 - learning_rate * dW1
         updated_parameters["W2"] = W2 - learning_rate * dW2
-        updated_parameters["db1"] = db1 - learning_rate * db1
-        updated_parameters["db2"] = db2 - learning_rate * db2
+        updated_parameters["b1"] = db1 - learning_rate * db1
+        updated_parameters["b2"] = db2 - learning_rate * db2
         return updated_parameters
 
     def initialize_parametres(self, n_input_units: int, n_hidden_units: int, n_output_units: int) -> dict:
@@ -84,4 +88,12 @@ class NeuralNetwork:
 
     def compute_cost(self, A_output: np.array, Y: np.array) -> float:
         m = A_output.shape[1]
-        return np.sum(Y * np.log(A_output) + (1 - Y) * np.log(1 - A_output)) / m
+        return -np.sum(Y * np.log(A_output) + (1 - Y) * np.log(1 - A_output)) / m
+
+
+if __name__ == "__main__":
+    X, Y = load_planar_dataset()
+    model = NeuralNetwork(n_hidden_units=4)
+    model.train_model(X, Y, n_epoch=10**5, learning_rate=0.1)
+    plot_decision_boundary(lambda x: model.predict(x), X, Y)
+    plt.show()
